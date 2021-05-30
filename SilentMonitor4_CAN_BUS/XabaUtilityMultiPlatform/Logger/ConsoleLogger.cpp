3d2e02ab7354f7ead41cfb3ffa8bd3663262230c
ideic <ideic75@gmail.com>
2021-05-30 14:01:15 +0200

    Console leogger logs the date and thread id

--- SilentMonitor4_CAN_BUS/XabaUtilityMultiPlatform/Logger/ConsoleLogger.cpp
+++ SilentMonitor4_CAN_BUS/XabaUtilityMultiPlatform/Logger/ConsoleLogger.cpp
@@ -1,10 +1,19 @@
 #include "ConsoleLogger.h"
 #include <iostream>
+#include <thread>
+#include <sstream>
+#include <DateTimeProvider.h>
 using namespace Xaba;
+using namespace std::string_literals;
+
 void ConsoleLogger::Info(const std::string& message) {
-	std::cout << message << '\n';
+	std::stringstream ss;
+	ss << std::this_thread::get_id();
+	std::cout << DateTimeProvider::DateTme() << " ["s << ss.str() << "] [INFO] "s << message << "\n";
 }
 
 void ConsoleLogger::Error(const std::string& message) {
-	std::cerr << message << '\n';
+	std::stringstream ss;
+	ss << std::this_thread::get_id();
+	std::cerr << DateTimeProvider::DateTme() << " ["s << ss.str() <<  "] [ERROR] "s << message << "\n";
 }
